1f3aa25634d9680d10d8d2e8fd8ca05022cd37e1
juanManuel802 <ju.ma.barrera.rivera@unillanos.edu.co>
2026-05-17 13:33:00 -0500

    Ahora con sessionStoragegit add .!

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,10 +3,23 @@ import { Login } from "./components/login";
 import { Home } from "./components/home";
 
 export default function App() {
-  const [currentUser, setCurrentUser] = useState<string | null>(null);
+  const [currentUser, setCurrentUser] = useState<string | null>(
+    sessionStorage.getItem("userId")
+  );
+
+  const handleLogin = (userId: string) => {
+    sessionStorage.setItem("userId", userId);
+    setCurrentUser(userId);
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("userId");
+    setCurrentUser(null);
+  };
+
   return currentUser ? (
-    <Home userId={currentUser} onLogout={() => setCurrentUser(null)} />
+    <Home userId={currentUser} onLogout={handleLogout} />
   ) : (
-    <Login onLogin={(userId) => setCurrentUser(userId)} />
+    <Login onLogin={handleLogin} />
   );
 }
